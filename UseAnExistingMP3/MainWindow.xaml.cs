13fb609373d5ce06c4b15d67e1bd24aadaa245c2
Rod Falanga <rfalanga@users.noreply.github.com>
2020-05-16 07:21:59 -0600

    put in some exception handling

--- UseAnExistingMP3/MainWindow.xaml.cs
+++ UseAnExistingMP3/MainWindow.xaml.cs
@@ -17,7 +17,14 @@ namespace UseAnExistingMP3
             InitializeComponent();
 
             mediaPlayer = new MediaPlayer();
-            mediaPlayer.Open(new Uri(Environment.CurrentDirectory + @"\Assets\Doctor_Who_-_Gold_Abridged.mp3", UriKind.RelativeOrAbsolute));
+            try
+            {
+                mediaPlayer.Open(new Uri(Environment.CurrentDirectory + @"\Assets\Doctor_Who_-_Gold_Abridged.mp3", UriKind.RelativeOrAbsolute));
+            }
+            catch (Exception)
+            {
+                // Continue even if there's a problem finding the .MP3 file
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -28,9 +35,14 @@ namespace UseAnExistingMP3
         //This helper function isn't necessary
         private void PlayTheSound()
         {
-            //Debug.WriteLine($"Before trying to play sound {mediaPlayer.HasAudio}"); //this was True
-            mediaPlayer.Play();
-            //Debug.WriteLine($"After trying to play sound {mediaPlayer.HasAudio}");  //this was True
+            if (mediaPlayer.HasAudio)
+            {
+                //Only play the audio, if we successfully opened the .MP3 file
+
+                //Debug.WriteLine($"Before trying to play sound {mediaPlayer.HasAudio}"); //this was True
+                mediaPlayer.Play();
+                //Debug.WriteLine($"After trying to play sound {mediaPlayer.HasAudio}");  //this was True
+            }
         }
     }
 }
